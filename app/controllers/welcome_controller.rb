ccaa49c06ca811f3edca27de4e1f289a0d5396c2
Jon Cheng <jon.syndicate@gmail.com>
2015-03-01 03:37:59 -0500

    added a bunch of svgjs stuff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    # `echo test | mail -s "testing" jon.syndicate@gmail.com`
   end
 end
---
+++ db/migrate/20150227233947_create_requests.rb
@@ -0,0 +1,10 @@
+class CreateRequests < ActiveRecord::Migration
+  def change
+    create_table :requests do |t|
+      t.text :email
+      t.text :name
+
+      t.timestamps
+    end
+  end
+end
