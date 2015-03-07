306cd6defb4e361883204c508fb5b8b238c335f1
Jon Cheng <jon.syndicate@gmail.com>
2015-03-07 00:57:23 -0500

    setup basic auth for rails admin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,14 @@
 class WelcomeController < ApplicationController
   def index
-    # `echo test | mail -s "testing" jon.syndicate@gmail.com`
+    # blah = "hello world!"
+    # hey = "echo test heres some stuff \n blah blah" +
+    # "asdfawefoih \n awef" +
+    # "awef awef aewf" + 
+    # "fwaef" + 
+    # "ef " +
+    # "#{blah}" +
+    #  "| mail -s 'homepage hit' jon.syndicate@gmail.com"
+
+    # `#{hey}`
   end
 end
