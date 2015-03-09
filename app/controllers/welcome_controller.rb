9874c0b3f9505de9a0edc999b4ebf9bbc038f664
Jon Cheng <jon.syndicate@gmail.com>
2015-03-08 23:00:16 -0400

    added lily

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    # blah = "hello world!"
-    # hey = "echo test heres some stuff \n blah blah" +
-    # "asdfawefoih \n awef" +
-    # "awef awef aewf" + 
-    # "fwaef" + 
-    # "ef " +
-    # "#{blah}" +
-    #  "| mail -s 'homepage hit' jon.syndicate@gmail.com"
-
-    # `#{hey}`
+    # puts "attempting to send mail"
+    # `echo "whatever man" | mail -v -s "some new ish" jon.syndicate@gmail.com`
   end
 end
