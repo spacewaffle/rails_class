68100cbf60c98077ecba7dd827250913f5d01221
Jon Cheng <jon.syndicate@gmail.com>
2015-03-19 15:24:48 -0400

    setup syllabus page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
   def index
     # puts "attempting to send mail"
   end
+
+  def syllabus
+    render layout: "short_form"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get 'efQ9' => 'welcome#syllabus'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
